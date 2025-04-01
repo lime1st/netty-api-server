b75982cdef88ded6d2a97fa0a9a0b1088adc5cdf
lime1st <lime1st@gmail.com>
2025-04-01 15:36:41 +0900

    refactor: 초기 데이터 입력 삭제

--- src/main/java/lime1st/netty/ApiServerApplication.java
+++ src/main/java/lime1st/netty/ApiServerApplication.java
@@ -1,45 +1,15 @@
 package lime1st.netty;
 
 import lime1st.netty.bootstrap.ApiServer;
-import lime1st.netty.domain.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ApiServerApplication {
 
     private static final Logger log = LoggerFactory.getLogger(ApiServerApplication.class);
-    private static boolean initialized = false;
-
-    private final UserRepository userRepository;
-
-    public ApiServerApplication(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
 
     public static void main(String[] args) throws Exception {
-        ApiServerApplication app = new ApiServerApplication(new UserRepository());
-        app.initializeData();
         ApiServer server = new ApiServer(8080, 1, Runtime.getRuntime().availableProcessors());
         server.start();
-    }
-
-    private synchronized void initializeData() {
-        if (!initialized) {
-            log.info("Initializing data...");
-            initUsers();
-            initialized = true;
-            log.info("Initialized data");
-        }
-    }
-
-    private synchronized void initUsers() {
-        try {
-            userRepository.insertUser(1L, "alex@mail.com", "alex", "passworda");
-            userRepository.insertUser(2L, "bred@mail.com", "bred", "passwordb");
-            userRepository.insertUser(2L, "ched@mail.com", "ched", "passwordc");
-            userRepository.insertUser(2L, "dave@mail.com", "dave", "passwordd");
-        } catch (Exception e) {
-            log.error("Error initializing users", e);
-        }
     }
 }
